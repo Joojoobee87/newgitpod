2371c72e15ac16e83b7497fa6bf35be796e6feb5
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-10-31 14:18:59 +0000

    amended jquery for slideshow

--- jquery.js
+++ jquery.js
@@ -26,22 +26,51 @@ $(document).ready(function() {
     function slideshow() {
 
         let images = Array.from($(".modal-image > img"));
-        let imageButton = $(".image-button");
-        let index = 0;
+        let nextButton = $(".next");
+        let prevButton = $(".prev");
+        let index = 1;
 
-        console.log(imageButton);
-        console.log(images);
+        $(images[0]).css("display", "block");
 
-        $(imageButton).on('click', function() {
+        $(nextButton).on('click', function() {
             for (i = 0; i < images.length; i++) {
-                console.log($(images[i]));
                 $(images[i]).css("display", "none");
             }
             index++;
-            if (index > images.length) { index = 1 }
+            if (index > images.length) {
+                $(images[images.length - 1]).css("display", "block");
+                return false;
+            }
             $(images[index - 1]).css("display", "block");
         })
-
+        $(prevButton).on('click', function() {
+            for (i = 0; i < images.length; i++) {
+                $(images[i]).css("display", "none");
+                console.log(images[i]);
+            }
+            index++;
+            console.log(index);
+            if (index < 0) {
+                $(images[images.length - i]).css("display", "block");
+            }
+            $(images[index - 1]).css("display", "block");
+        })
     }
     slideshow();
+
 })
+function nextImage() {
+    if (index < images.length - 1) {
+        for (i = 0; i < 10; i++) {
+            document.getElementById('imgsrc').src = images[index++];
+        }
+    }
+}
+
+function previousImage() {
+    if (index > 0) {
+        for (i = 0; i < 10; i++) {
+            document.getElementById('imgsrc').src = images[index--];
+        }
+    }
+}
