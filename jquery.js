625e28a0f9847840ac15253f3e9ba0bf2b3573cc
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-10-31 15:04:45 +0000

    finalised jquery for next and previous functions when slide buttons are clicked

--- jquery.js
+++ jquery.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
     }
     filterSelected();
 
-
     function slideshow() {
 
         let images = Array.from($(".modal-image > img"));
@@ -37,42 +36,27 @@ $(document).ready(function() {
                 $(images[i]).css("display", "none");
             }
             index++;
-            if (index > images.length) {
+            console.log(index)
+            if (index > images.length - 1) {
                 $(images[images.length - 1]).css("display", "block");
+                index = 0;
                 return false;
             }
             $(images[index - 1]).css("display", "block");
         })
 
         $(prevButton).on('click', function() {
-            console.log(images);
             for (i = 0; i < images.length; i++) {
                 $(images[i]).css("display", "none");
-                console.log(images[i]);
             }
             index--;
             console.log(index);
-            if (index < 0) {
-                $(images[images.length - i]).css("display", "block");
+            if (index < 1) {
+                $(images[images.length+1]).css("display", "block");
+                index = images.length
             }
-            $(images[index - 1]).css("display", "block");
+            $(images[index-1]).css("display", "block");
         })
     }
     slideshow();
-
 })
-function nextImage() {
-    if (index < images.length - 1) {
-        for (i = 0; i < 10; i++) {
-            document.getElementById('imgsrc').src = images[index++];
-        }
-    }
-}
-
-function previousImage() {
-    if (index > 0) {
-        for (i = 0; i < 10; i++) {
-            document.getElementById('imgsrc').src = images[index--];
-        }
-    }
-}
